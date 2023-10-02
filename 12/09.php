08dfe5fc49f7f080a3a95d3d6e8e2242858d161f
RobinNixon <robin@robinnixon.com>
2023-10-02 14:36:34 +0100

    Updated

--- 12/09.php
+++ 12/09.php
@@ -1,8 +1,7 @@
 <?php
   function sanitizeString($var)
   {
-    if (get_magic_quotes_gpc())
-      $var = stripslashes($var);
+    $var = stripslashes($var);
     $var = strip_tags($var);
     $var = htmlentities($var);
     return $var;
--- 12/10.php
+++ 12/10.php
@@ -38,8 +38,7 @@ _END;
 
 function sanitizeString($var)
 {
-  if (get_magic_quotes_gpc())
-    $var = stripslashes($var);
+  $var = stripslashes($var);
 	$var = htmlentities($var);
 	$var = strip_tags($var);
 	return $var;
