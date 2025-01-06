7fbc3b9b392e77cbfa9c966e090f2a393b7c87de
Michal Špaček <mail@michalspacek.cz>
2025-01-06 18:21:10 +0100

    Replace tabs with spaces

--- 04/21.php
+++ 04/21.php
@@ -1,17 +1,17 @@
 <?php
   if ($bank_balance < 100)
   {
-  	$money         = 1000;
-  	$bank_balance += $money;
+    $money         = 1000;
+    $bank_balance += $money;
   }
   elseif ($bank_balance > 200)
   {
-  	$savings      += 100;
-  	$bank_balance -= 100;
+    $savings      += 100;
+    $bank_balance -= 100;
   }
   else
   {
-  	$savings      += 50;
-  	$bank_balance -= 50;
+    $savings      += 50;
+    $bank_balance -= 50;
   }
 ?>
