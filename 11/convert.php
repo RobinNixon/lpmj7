8419a55797e7e92827c490cb37d82ad0ba6987bb
Michal Špaček <mail@michalspacek.cz>
2025-01-06 02:42:06 +0100

    Updated ch11 examples

--- 11/convert.php
+++ 11/convert.php
@@ -1,22 +1,24 @@
 <?php // convert.php
-  $f = $c = '';
+  $f = $c = $f_html_entities = $c_html_entities = '';
 
-  if (isset($_POST['f'])) $f = sanitizeString($_POST['f']);
-  if (isset($_POST['c'])) $c = sanitizeString($_POST['c']);
+  if (isset($_POST['f'])) {
+    $f = $_POST['f'];
+    $f_html_entities = htmlentities($f);
+  }
+  if (isset($_POST['c'])) {
+    $c = $_POST['c'];
+    $c_html_entities = htmlentities($c);
+  }
 
-  if (is_numeric($f))
-  {
+  if (is_numeric($f)) {
     $c = intval((5 / 9) * ($f - 32));
-    $out = "$f &deg;f equals $c &deg;c";
-  }
-  elseif(is_numeric($c))
-  {
+    $out = "$f &deg;F equals $c &deg;C";
+  } elseif(is_numeric($c)) {
     $f = intval((9 / 5) * $c + 32);
-    $out = "$c &deg;c equals $f &deg;f";
-  }
-  else $out = "";
-
-  echo <<<_END
+    $out = "$c &deg;C equals $f &deg;F";
+  } else
+    $out = "";
+?>
 <html>
   <head>
     <title>Temperature Converter</title>
@@ -24,23 +26,15 @@
   <body>
     <pre>
       Enter either Fahrenheit or Celsius and click on Convert
-        
-      <b>$out</b>
-      <form method="post" action="convert.php">
-        Fahrenheit <input type="text" name="f" size="7">
-           Celsius <input type="text" name="c" size="7">
+
+      <b><?php echo $out; ?></b>
+      <form method="post" action="">
+        <label>Fahrenheit <input type="text" name="f"
+          value="<?php echo $f_html_entities; ?>" size="7"></label>
+           <label>Celsius <input type="text" name="c"
+              value="<?php echo $c_html_entities; ?>" size="7"></label>
                    <input type="submit" value="Convert">
       </form>
     </pre>
   </body>
 </html>
-_END;
-
-function sanitizeString($var)
-{
-  var = stripslashes($var);
-	$var = htmlentities($var);
-	$var = strip_tags($var);
-	return $var;
-}
-?>
--- 11/formtest2.php
+++ 11/formtest2.php
@@ -1,20 +1,20 @@
 <?php // formtest2.php
-  if (!empty(($_POST['name']))) $name = $_POST['name'];
+  if (!empty(($_POST['name']))) $name = htmlentities($_POST['name']);
   else $name = "(Not Entered)";
 
   echo <<<_END
-  <html>
-    <head>
-      <title>Form Test</title>
-    </head>
-    <body>
-    Your name is: $name<br>
-    <form method="post" action="formtest2.php">
-      What is your name?
-      <input type="text" name="name">
-      <input type="submit">
-    </form>
-    </body>
-  </html>
+    <html>
+      <head>
+        <title>Form Test</title>
+      </head>
+      <body>
+        Your name is: $name<br>
+        <form method="post" action="formtest2.php">
+          What is your name?
+          <input type="text" name="name">
+          <input type="submit">
+        </form>
+      </body>
+    </html>
 _END;
 ?>
